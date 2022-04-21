024cdfd16d431267727ac4252485e79ad65a816c
nyaribari <anyaribari@gmail.com>
2022-04-21 14:07:43 +0300

    adding test_cli_app file

---
+++ test_cli_app.py
@@ -0,0 +1,13 @@
+import unittest
+from lyrics_finder import create_musixmatch_database, load_song, lyrics_finder
+
+class TestCLIApp(unittest.TestCase):
+
+    def test_create_musixmatch_db(self):
+        pass
+
+    def test_load_song(self):
+        pass
+
+    def test_lyrics_finder(self):
+        pass
