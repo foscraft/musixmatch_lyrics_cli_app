3f55dd719a2698cd95736e91d914ce9f60c1517c
nyaribari <anyaribari@gmail.com>
2022-04-22 19:04:06 +0300

    updating tests and adding more Mock tests for sqlite3

--- test_cli_app.py
+++ test_cli_app.py
@@ -1,4 +1,7 @@
+from calendar import c
+from re import M
 import unittest
+import sqlite3
 from unittest.mock import MagicMock, Mock 
 from lyrics_finder import  lyrics_finder
 from create_database import create_database,create_table, load_song
@@ -6,13 +9,24 @@ from create_database import create_database,create_table, load_song
 class CliAppTests(unittest.TestCase):
 
     def test_create_db(self):
-        pass
+
+        sqlite3.connect = MagicMock(return_value='connection successful')
+
+        dbc = create_database()
+        sqlite3.connect.assert_called_with('my_database')
+        self.assertEqual(dbc,'connection successful')
 
     def test_create_table(self):
-        pass
+        conn = sqlite3.connect = MagicMock(return_value='table_created')
+        tb = create_table(conn)
+        conn.assert_called_with('lyrics_table')
+        self.assertEqual(tb,"table_created")
 
     def test_load_song(self):
         pass
 
     def test_lyrics_finder(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()
