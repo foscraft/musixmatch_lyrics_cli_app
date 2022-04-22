3e7cc30694839ef4843df46085d8f85ca9db31b0
nyaribari <anyaribari@gmail.com>
2022-04-22 18:29:21 +0300

    create database function, create table function and tests

--- create_database.py
+++ create_database.py
@@ -2,9 +2,9 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_musixmatch_database():
+def create_database():
     """
-    Create a new database and lyrics table for the application
+    Create a new database and 
     -> Pass a name of choice for your database below and 
     table name of your choice.
     Both will be created on the fly.
@@ -14,16 +14,24 @@ def create_musixmatch_database():
         conn = sqlite3.connect('my_database')
     except Error as e:
         print(e)
+    return conn
+
+
+def create_table(conn):
+    '''
+    Creating the lyrics table
+    '''
     c = conn.cursor()
     c.execute('''
             CREATE TABLE IF NOT EXISTS lyrics_table
             ([lyrics] TEXT)
             ''')                 
     conn.commit()
-   
-def load_song(song):
 
-    conn = sqlite3.connect('my_database') 
+   
+def load_song(conn,song):
+    '''
+    Loading  the song into the table'''
     c = conn.cursor()
     sql = ''' INSERT INTO lyrics_table VALUES(?) '''
     c.execute(sql, [song])             
